2192a0cf6dda356e41cf4f8c96e8dd02a7b052c1
karlobob <karlo.jagape@gmail.com>
2026-03-24 01:53:55 -0400

    Added resources

--- day-3-challenge/main.tf
+++ day-3-challenge/main.tf
@@ -3,10 +3,29 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-02dfbd4ff395f2a1b"
-  instance_type = "t3.small"
+  ami                    = "ami-0ec10929233384c7f" # ami-0fb653ca2d3203ac1 (Amazon Linux 2 not available anymore)
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data                   = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        # converted to Amazon2023 which is httpd compatible
+        nohup python3 -m http.server 8080 &
+        EOF
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
